697de71b5c23c5790a7c8067adfba580e197e9bf
ricrol <rich.roller@gmail.com>
2016-08-14 15:32:15 -0400

    second update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='parse_checks',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/rollerr/parse_checks',
+      author='rollerr',
+      author_email='notyet@aol.com',
+      license='MIT',
+      packages=['parse_checks'],
+      zip_safe=False)
